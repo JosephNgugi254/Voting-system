160dc0c7ded5f4a39ab8f0586c0d1ca07e9745d5
CliffLinusthomi <linusthomi2@gmail.com>
2025-04-23 12:17:13 +0300

    catch up

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+// import React, {useState, useEffect} from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Home from "./components/Home";
+// import VotingPage from "./components/VotingPage";
+// import ResultsPage from "./components/ResultsPage";import { useEffect } from "react"; 
+
+   
